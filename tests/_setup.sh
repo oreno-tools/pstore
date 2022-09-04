004e7b0dec8a90ca66889febbe4c2142782bc406
inokappa <inokara@gmail.com>
2022-09-04 11:05:35 +0900

    fix

--- tests/_setup.sh
+++ tests/_setup.sh
@@ -4,4 +4,4 @@ aws --endpoint=http://127.0.0.1:5000 \
   ssm put-parameter \
     --name 'testtest' \
     --type 'String' \
-    --value '12345678901234567890' > /dev/null
+    --value '12345678901234567890'
